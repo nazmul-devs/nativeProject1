33f77f2bde51b5fa22e3c4ecdfed962a8fe7aa6d
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-12-17 18:39:07 +0600

    starting page created

--- Component/Home.js
+++ Component/Home.js
@@ -1,21 +1,46 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
 import Tabs from "./Tabs";
 
 function Home(props) {
 	return (
 		<View style={styles.container}>
-			<Text>Hello world</Text>
-			<Tabs />
+			<Text style={styles.mainText}>StyleHut</Text>
+			<Image
+				style={styles.bannerImg}
+				source={require("../assets/images/home-banner.png")}
+			></Image>
+			<Text style={styles.startText}>Get start ➨</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
+	bannerImg: {
+		width: "100%",
+	},
 	container: {
 		backgroundColor: "#ccc",
 		flex: 1,
 		width: "100%",
+		justifyContent: "space-evenly",
+	},
+	mainText: {
+		color: "#212F3C",
+		fontSize: 32,
+		fontWeight: "bold",
+		marginTop: 30,
+		textAlign: "center",
+	},
+	startText: {
+		backgroundColor: "dodgerblue",
+		fontSize: 30,
+		fontWeight: "bold",
+		padding: 10,
+		color: "#fff",
+		textAlign: "center",
+		margin: 10,
+		borderRadius: 10,
 	},
 });
 
